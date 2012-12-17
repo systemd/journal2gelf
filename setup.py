d9adbefa1d174a12e68852ffb4b92df3ccecd436
Nick Stielau <nick.stielau@gmail.com>
2012-12-17 12:44:07 -0800

    Bumping version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='journal2gelf',
-    version='0.0.1',
+    version='0.0.2',
     author='Joe Miller',
     author_email='joeym@joeym.net',
     scripts=['journal2gelf'],
